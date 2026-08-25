d2114b2934643a14abad7722a78275f61bc08308
jesgilcan-hue <jesgilcan@gmail.com>
2026-08-25 16:08:10 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        habla: resolve(__dirname, 'habla-claro.html'),
         deja: resolve(__dirname, 'deja-de-darle-mil-vueltas.html'),
         p90: resolve(__dirname, 'planificador-90-dias.html'),
         p180: resolve(__dirname, 'planificador-180-dias.html'),
